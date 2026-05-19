3064be544d26f32d2341427c7f75753e16411641
asif0257 <114487969+asif0257@users.noreply.github.com>
2026-05-19 13:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Hello from Jenkins + GCP + ansible CI/CD pipeline!",
+        "message": "Hello from Jenkins + GCP + ansible + test branch CI/CD pipeline!",
         "environment": os.getenv("APP_ENV", "production"),
         "version": os.getenv("BUILD_NUMBER", "1")
     })
